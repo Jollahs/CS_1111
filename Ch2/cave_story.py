0cc2624e79c3ef9f1252973b588fcff353147539
norkish <norkish@gmail.com>
2025-09-04 15:35:29 -0600

    Create cave_story.py

---
+++ Ch2/cave_story.py
@@ -0,0 +1,6 @@
+print('''You reach a cave. You can either:
+\tA. Go in the cave
+\tB. Shout into the cave
+\tC. Wait outside''')
+choice = input("What do you choose? ")
+print("You chose " + choice + ".")
