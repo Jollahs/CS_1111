35bf6a3ed662b2c9c106c8474676683db82f92d3
norkish <norkish@gmail.com>
2025-09-04 15:37:07 -0600

    Create birthday.py

---
+++ Ch2/birthday.py
@@ -0,0 +1,14 @@
+#Using valid variable names write a program that creates variables for
+# a 1) birth month,
+birth_month = "March"
+
+# 2) birth date, and 3) birth year.
+birth_day = 3
+
+birth_year = 1980
+
+
+# Then print it out using the format  ”January 1, 2000”
+print(birth_month, birth_day, ",", birth_year)
+
+print(birth_month + " " + str(birth_day) + ", " + str(birth_year))
