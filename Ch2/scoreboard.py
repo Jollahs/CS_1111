c4372e006b5f69d07a14eafd427ceb7d28b62243
norkish <norkish@gmail.com>
2025-09-04 15:37:31 -0600

    Create scoreboard.py

---
+++ Ch2/scoreboard.py
@@ -0,0 +1,13 @@
+# Write a program that has a variable called score whose starting value is 0.
+score = 0
+
+# Print the variable score.
+print("The score is", score)
+
+# Change the value of score and print it.
+score = 3
+print("The score is", score)
+
+# Change and print score again.
+score = 5
+print("The final score is", score)
