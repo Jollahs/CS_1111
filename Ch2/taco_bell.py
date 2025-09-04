d9ed60d20b07fad5022ff10e9377cfcb575097f6
norkish <norkish@gmail.com>
2025-09-04 15:37:53 -0600

    Create taco_bell.py

---
+++ Ch2/taco_bell.py
@@ -0,0 +1,11 @@
+item_1_name = "Sausage Burrito"
+item_1_quantity = 2
+item_1_price = 3.58
+
+print(item_1_quantity, item_1_name, item_1_price)
+
+item_2_name = "Mild Picante"
+item_2_quantity = 1
+item_2_price = 0.39
+
+print(item_2_quantity, item_2_name, item_2_price)
