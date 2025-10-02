5e00e2eb5eb4f62a283ea335fd118d52bbff019b
norkish <norkish@gmail.com>
2025-10-02 12:42:53 -0600

    Create calculate_difference.py

---
+++ ch6/calculate_difference.py
@@ -0,0 +1,8 @@
+def calculate_difference(a, b):
+    """Calculate and print the absolute value of the difference between a and b."""
+    print(abs(a-b))
+
+
+calculate_difference(5, 6)
+calculate_difference(3, 6)
+calculate_difference(-9, 10)
