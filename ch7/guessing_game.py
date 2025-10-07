abc09a8ce30b8dfcd28b2d5a2294a1a9323b0e36
norkish <norkish@gmail.com>
2025-10-07 15:12:42 -0600

    Create guessing_game.py

---
+++ ch7/guessing_game.py
@@ -0,0 +1,16 @@
+# Design and write a program that assigns a number
+# (chosen by you) between 1 and 10 to a variable
+real_number = 5
+
+# then prompts the user to guess the number.
+user_guess = int(input("Please guess an integer between 1 and 10, inclusive: "))
+
+# Print out “True” or “False” based on whether
+# the guessed number equaled the number you assigned.
+print(real_number == user_guess)
+
+# if user_guess == real_number:
+#     print("Bingo! Yahtzee! You guessed it!")
+
+# Adapt the program to give the user 5 guesses.
+
