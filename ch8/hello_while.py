1989d4a607e6ca901892754aba68013bf131f9a5
norkish <norkish@gmail.com>
2025-10-14 15:02:45 -0600

    Create hello_while.py

---
+++ ch8/hello_while.py
@@ -0,0 +1,7 @@
+score = int(input("What was your score?"))
+
+while score < 50:
+	print("Your score was too low. Try again.")
+	score = int(input("What was your score?"))
+
+print("Well done! You passed!")
