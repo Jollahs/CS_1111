75cf9a4b6cd49c4b60528b61340bfccd63d3426c
norkish <norkish@gmail.com>
2025-10-14 15:01:46 -0600

    Create for_to_while.py

---
+++ ch8/for_to_while.py
@@ -0,0 +1,21 @@
+print("FOR LOOP PART 1")
+for i in range(5):
+    print("Hello", i)
+
+print("WHILE LOOP VERSION")
+
+i = 0 # loop variable initialization
+while i < 5: # loop condition
+    print("Hello", i)
+    i += 1 # loop variable update
+
+print("FOR LOOP PART 2")
+
+for i in range(5,10,2):
+    print("Hello", i)
+
+print("WHILE LOOP VERSION")
+i = 5 # loop variable initialization
+while i < 10: # loop condition
+    print("Hello", i)
+    i += 2 # loop variable update
